63ce8bd467d742dfe6dc1af37d5bd402307ed8ff
TaylorSwiftlove <alswl5436@naver,com>
2021-01-26 21:49:42 +0900

    a.java

---
+++ a.java
@@ -0,0 +1,27 @@
+public class a{
+
+    public static int sum(int n, int m){
+        return n+m;
+    }
+
+    public static void main(String[] args){
+        int i =20;
+        int s;
+        char a;
+        String b, c;
+        String first = "coding";
+
+        System.out.println(first+"everytime");
+        s = sum(i, 10);a = '?';
+        System.out.println(a);
+        System.out.println("Hello");
+        System.out.println(s);
+        System.out.println("생활"+"코딩"); //문자열은 연산도 가능하다
+        b = "coding";
+        c = " everytime";
+        System.out.println(a+b);
+
+        //; 한구문이 끝남
+
+    }
+}
