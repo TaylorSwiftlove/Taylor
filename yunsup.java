5e1f6fa8956f14a64b5e32726c84f8a743390703
TaylorSwiftlove <alswl5436@naver.com>
2021-05-23 19:22:26 +0900

    [yunsup.java]

---
+++ yunsup.java
@@ -0,0 +1,14 @@
+public class yunsup {
+    //메소드
+    public static int plus(int i){
+        for(int j=0; j<=5; j++){
+            i++;
+        }
+        return i;
+    }
+    //메인 메소드
+    public static void main(String[] args){
+        System.out.println(plus(5));
+
+    }
+}
